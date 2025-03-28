c1dfb81b4b795e694f5a25c948b2eb8d90c9f1d6
PrinceRaj8 <princeraj7g5@gmail.com>
2025-03-28 13:56:52 +0530

    Update database.py

--- face_attendance_backend/database.py
+++ face_attendance_backend/database.py
@@ -1,7 +1,9 @@
+import os
 import psycopg2
 
-DATABASE_URL = "postgresql://your_user:your_password@your_host:your_port/your_db"
+DATABASE_URL = os.getenv("DATABASE_URL")  # Render से Database URL मिलेगा
 
 def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URL)
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     return conn
+
